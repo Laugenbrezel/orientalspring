676f8f401b1bb3ffaf434ad3b7291809132c5a63
Lutz Zimmermann <lutz.zimmermann@sva.de>
2015-07-09 01:34:22 +0200

    fixed tests

--- src/test/java/demo/OrientalspringApplicationTests.java
+++ src/test/java/demo/OrientalspringApplicationTests.java
@@ -2,40 +2,41 @@ package demo;
 
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
-import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.ops4j.orient.spring.tx.OrientBlueprintsGraphFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = OrientalspringApplication.class)
-@WebAppConfiguration
-public class OrientalspringApplicationTests {
+@ContextConfiguration(classes = OrientalspringApplication.class)
+@TestExecutionListeners({TransactionalTestExecutionListener.class})
+@Transactional
+public class OrientalspringApplicationTests extends AbstractJUnit4SpringContextTests {
 
-	@Autowired
-	OrientGraphFactory orientGraphFactory;
+    @Autowired
+    OrientBlueprintsGraphFactory orientGraphFactory;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
-	@Test
-	public void testFactoryInitialized() throws Exception {
-		assertNotNull(orientGraphFactory);
+    @Test
+    public void testFactoryInitialized() throws Exception {
+        assertNotNull(orientGraphFactory);
 
-        OrientGraph graph = orientGraphFactory.getTx();
+        OrientGraph graph = orientGraphFactory.graph();
         assertNotNull(graph);
 
         Map<String, Object> values = new HashMap<>();
-        try{
+        try {
             values.put("name", "Luca");
             Vertex luca = graph.addVertex("class:Person", values);
             values.put("name", "Marko");
@@ -44,7 +45,7 @@ public class OrientalspringApplicationTests {
             graph.commit();
 
             graph.addEdge(null, luca, marko, "knows");
-        } catch( Exception e ) {
+        } catch (Exception e) {
             graph.rollback();
             throw new RuntimeException(e);
         }
